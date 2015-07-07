da80e1dde1ab1dac21049fa74039819682e32b6d
David Rosson <david@rosson.com.au>
2015-07-06 21:09:44 -0700

    a working version

--- solve.js
+++ solve.js
@@ -1,49 +1,64 @@
-var brute = function() {
-  // start by placing the first queen
-  for (var i=0; i<size; i++) {
-    for (var j=0; j<size; j++) {
-      pos = '#'+i+''+j;
-      cell = $(pos);
-      setQueen(cell);
-    }
-  }
-};
-
 var solve = function (rows, columns) {
   var y = rows - 1;
-  if (rows && columns) {
+  if (rows > 0) {
     return placeQueen(y, columns);
+  } else {
+    return [[]];
   }
 };
  
-var placeQueen = function (y, columns, prevSolution) {
+var placeQueen = function (y, columns) {
   var out = [],
-      cell, coordinate,
+      cell, coordinate;
       // kick off the recursive run
+      console.log('calling solve ' + y + ', ' + columns);
       playbook = solve(y, columns);
+      console.log('playbook: ', JSON.stringify(playbook));
     
     for (var i = 0; i < playbook.length; i++) {
 
-      var place = playbook[i];
+      var play = playbook[i];
+      console.log('item ' + i + ' in playbook: ' + JSON.stringify(play));
 
       for (var x = 0; x < columns; x++) {
-        if (!death(y, x, place)) {
-          out.push(place.concat([x]));
-          coordinate = '#' + i + '' + x;
-          console.log(coordinate);
-          cell = $(coordinate);
-          setQueen(cell);
+
+        console.log('testing conflict: ' + y + ', ' + x + ' : ' + death(y, x, play));
+        
+        if (!death(y, x, play)) {
+          out.push(play.concat([x]));
         }
       }
-// http://rosettacode.org/wiki/N-queens_problem#Javascript
     }
-
+    console.log('output: ', JSON.stringify(out));
+    render(out);
     return out;
 };
+
+var clear = function (size) {
+  var coordinate, cell;
+  for (var i = 0; i < size; i++) {
+    for (var j =0; j < size; j++) {
+    coordinate = '#' + i + '' + j;
+    cell = $(coordinate);
+    removeQueen(cell);
+    }
+  }
+};
+
+var render = function (playbook) {
+  clear(size);
+  var play = playbook[0];
+  var coordinate, cell;
+  for (var i=0; i<play.length; i++) {
+    coordinate = '#' + i + '' + play[i];
+    cell = $(coordinate);
+    setQueen(cell);
+  }
+};
  
-var death = function (y, x, place) {
+var death = function (y, x, play) {
   for (var i = 0; i < y; i++) {
-    if ( place[i] === x || place[i] + i === x + y || place[i] - i === x - y) {
+    if ( play[i] === x || play[i] + i === x + y || play[i] - i === x - y) {
       return true;
     }
   }
